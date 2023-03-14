8a46ff7ecabc3b999696101dc1f5ad1664015a48
Er1cYNShi <13222776992@163.com>
2023-03-14 18:44:49 -0400

    sql

--- server/index.js
+++ server/index.js
@@ -1,6 +1,29 @@
-require("dotenv").config();
-const express = require("express");
-// const SQL = 
-const cors = require("cors");
+const express = require('express');
+const mysql = require('mysql');
+
+// Create MySQL connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'yourusername',
+  password: 'yourpassword',
+  database: 'yourdatabase'
+});
+
+// Connect to MySQL database
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL database!');
+});
+
+// Create Express app. instance
 const app = express();
-// const PORT = 
+
+// Defining basic routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start server
+app.listen(3000, () => {
+  console.log('Server listening on port 3000!');
+});
