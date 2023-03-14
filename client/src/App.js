3ba5bcd57168ac9c1e5a48c0874ecdb57c4d416b
CatFaith <junyilin0185@gmail.com>
2023-03-14 17:51:16 -0400

    example

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,49 @@
 // APP Class
+/*
+import './App.css';
+import { React } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { AuthContextProvider } from './auth';
+import { GlobalStoreContextProvider } from './store'
+import {
+    AppBanner,
+    HomeWrapper,
+    RegisterScreen,
+    Statusbar,
+    WorkspaceScreen
+} from './components'
+import SignInSide from './components/SignInSide';
+*/
+/*
+    This is our application's top-level component.
+    
+    @author McKilla Gorilla
+*/
+/*
+  This is the entry-point for our application. Notice that we
+  inject our store into all the components in our application.
+  
+  @author McKilla Gorilla
+*/
+/*
+const App = () => {
+    return (
+        <BrowserRouter>
+            <AuthContextProvider>
+                <GlobalStoreContextProvider>              
+                    <AppBanner />
+                    <Switch>
+                        <Route path="/" exact component={HomeWrapper} />
+                        <Route path="/register/" exact component={RegisterScreen} />
+                        <Route path="/top5list/:id" exact component={WorkspaceScreen} />
+                        <Route path="/signin/" exact component={SignInSide}/>
+                    </Switch>
+                    <Statusbar />
+                </GlobalStoreContextProvider>
+            </AuthContextProvider>
+        </BrowserRouter>
+    )
+}
+
+export default App
+*/
