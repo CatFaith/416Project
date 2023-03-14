1157138a7e00115ba23eb27cb4848499b415d081
CatFaith <junyilin0185@gmail.com>
2023-03-14 18:30:25 -0400

    small change

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,11 @@
 // APP Class
-/*
-import './App.css';
+import './index.css';
 import { React } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { AuthContextProvider } from './auth';
-import { GlobalStoreContextProvider } from './store'
 import {
-    AppBanner,
-    HomeWrapper,
-    RegisterScreen,
-    Statusbar,
-    WorkspaceScreen
+    Drive,
+    Toolbar
 } from './components'
-import SignInSide from './components/SignInSide';
-*/
-
-/*
-    This is our application's top-level component.
-    
-    @author McKilla Gorilla
-*/
-
-/*
-  This is the entry-point for our application. Notice that we
-  inject our store into all the components in our application.
-  
-  @author McKilla Gorilla
-*/
 
 /*
 const App = () => {
