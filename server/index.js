300a15abe3aa72ad238f8e5c09bddec234d0990e
Er1cYNShi <13222776992@163.com>
2023-03-16 13:26:51 -0400

    Add 3 Modules, Coding index.js

--- server/index.js
+++ server/index.js
@@ -1,29 +1,44 @@
+require("dotenv").config();
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
+const cors = require("cors"); // 跨域请求 cross-origin requests
+const PORT = process.env.PORT || 4000;
 
-// Create MySQL connection
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'yourusername',
-  password: 'yourpassword',
-  database: 'yourdatabase'
-});
-
-// Connect to MySQL database
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to MySQL database!');
-});
 
 // Create Express app. instance
 const app = express();
 
-// Defining basic routes
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(cors());
+
+const usersRoutes = require('./modules/Users/routes');
+const appRoutes = require('./modules/APPs/routes');
+const tableRoutes = require('./modules/Views/routes');
+
+app.use('/api/Users', usersRoutes);
+app.use('/api/APPs', appRoutes);
+app.use('/api/Views', tableRoutes);
+
+
+
+// Create MySQL connection 未完成
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
-// Start server
-app.listen(3000, () => {
-  console.log('Server listening on port 3000!');
+
+// Connect to MySQL database
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database:', err);
+    return;
+  }
+  console.log('Connected to MySQL database');
+
+  //start server
+  app.listen(PORT, () => {
+    console.log(`The server set up at port: ${PORT}`);
+  });
 });
