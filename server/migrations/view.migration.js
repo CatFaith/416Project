5dde7891897a74ef78c7d52406665f8ba46a2d0a
Er1cYNShi <13222776992@163.com>
2023-04-24 11:27:22 -0400

    little changes

--- server/migrations/view.migration.js
+++ server/migrations/view.migration.js
@@ -37,6 +37,10 @@ module.exports = {
         type: Sequelize.STRING,
         comment:''
       },
+      editColumns: {
+        type: Sequelize.STRING,
+        comment:''
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
