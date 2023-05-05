b986f7fbcedb7394e34afaea7a5e814220b47ac9
YUANDAHE <yuanda.he@stonybrook.edu>
2023-05-05 00:48:39 -0400

    remove associate()

--- server/models/log.model.js
+++ server/models/log.model.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      // no associate with other models, it is independent.
     }
   }
   Log.init({
